d3e84a02914a5929aac6c15b6fd919a2193ecf00
sophiedannery <dannery.sophie@gmail.com>
2025-02-02 21:12:49 +0100

    ajout covoiturage back

--- ajout_covoiturage.php
+++ ajout_covoiturage.php
@@ -1,8 +1,44 @@
 <?php
 require_once 'templates/header.php';
 require_once 'lib/user.php';
+require_once 'lib/lib_ajout_covoiturage.php';
 require_once 'lib/pdo.php';
 
+//session est start dans le header
+
+//Rediriger quelqu'un s'il n'est pas connecté
+if (!isset($_SESSION["user"])) {
+    header('Location: connexion.php');
+    exit();
+}
+
+//logique ajout trajet
+$errors = [];
+$messages = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $tripData = [
+        //trim permet d'enlever les espaces blancs inutiles
+        // ?? '' permet de garantir que si un champ n'est pas rempli, une valeur vide sera utilisée à la place
+        'adresse_depart' => trim($_POST["adresse_depart"] ?? ''),
+        'adresse_arrivee' => trim($_POST["adresse_arrivee"] ?? ''),
+        'date_depart' => trim($_POST["date_depart"] ?? ''),
+        'heure_depart' => trim($_POST["heure_depart"] ?? ''),
+        'heure_arrivee' => trim($_POST["heure_arrivee"] ?? ''),
+        'prix' => trim($_POST["prix"] ?? ''),
+        'place_disponible' => trim($_POST["place_disponible"] ?? ''),
+    ];
+
+    //ajout à la bdd grâce à la fonction
+    $res = validateAndAddTrip($pdo, $_SESSION["user"]["id"], $tripData);
+
+    //message de succès ou d'erreur 
+    if ($res === true) {
+        $messages[] = "Trajet ajouté avec succès !";
+    } else {
+        $errors = $res;
+    }
+}
 
 ?>
 
@@ -15,40 +51,57 @@ require_once 'lib/pdo.php';
 <div class="container">
     <div class="text-center">
 
+        <?php if ($messages): ?>
+            <div class="alert alert-success">
+                <?= implode("<br>", $messages) ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($errors): ?>
+            <div class="alert alert-danger">
+                <?= implode("<br>", $errors) ?>
+            </div>
+        <?php endif; ?>
+
+
 
         <form method="post" class="p-4 p-md-4 border rounded-3 bg-body-tertiary">
             <div class="form-floating mb-3 ">
-                <input type="text" class="form-control" id="adresse_depart" name="adresse_depart">
+                <input type="text" class="form-control" id="adresse_depart" name="adresse_depart" value="<?= htmlspecialchars($_POST['adresse_depart'] ?? '') ?>">
                 <label for="adresse_depart">Ville de départ</label>
             </div>
             <div class="form-floating mb-3 ">
-                <input type="text" class="form-control" id="adresse_arrivee" name="adresse_arrivee">
+                <input type="text" class="form-control" id="adresse_arrivee" name="adresse_arrivee" value="<?= htmlspecialchars($_POST['adresse_arrivee'] ?? '') ?>">
                 <label for="adresse_arrivee">Ville d'arrivée'</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="date" class="form-control" id="date_depart" name="date_depart" value="<?= htmlspecialchars($_POST['date_depart'] ?? '') ?>">
+                <label for="date_depart">Date</label>
             </div>
             <div class="row g-3 mb-3">
                 <div class="col-md-6">
                     <div class="form-floating position-relative">
-                        <input type="date" class="form-control ps-5" id="date_depart" name="date_depart">
-                        <label for="date_depart">Date</label>
+                        <input type="time" class="form-control ps-5" id="heure_depart" name="heure_depart" value="<?= htmlspecialchars($_POST['heure_depart'] ?? '') ?>">
+                        <label for="heure_depart">Heure de départ</label>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-floating position-relative">
-                        <input type="time" class="form-control ps-5" id="heure_depart" name="heure_depart">
-                        <label for="heure_depart">Heure</label>
+                        <input type="time" class="form-control ps-5" id="heure_arrivee" name="heure_arrivee" value="<?= htmlspecialchars($_POST['heure_arrivee'] ?? '') ?>">
+                        <label for="heure_arrivee">Heure d'arrivée</label>
                     </div>
                 </div>
             </div>
             <div class="row g-3 mb-3">
                 <div class="col-md-6">
                     <div class="form-floating mb-3">
-                        <input type="number" class="form-control" id="place_disponible" name="place_disponible" min="0">
+                        <input type="number" class="form-control" id="place_disponible" name="place_disponible" min="0" value="<?= htmlspecialchars($_POST['place_disponible'] ?? '') ?>">
                         <label for="place_disponible">Nombre de passagers</label>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-floating mb-3">
-                        <input type="number" class="form-control" id="prix" name="prix" min="0" step="0.01">
+                        <input type="number" class="form-control" id="prix" name="prix" min="0" step="0.01" value="<?= htmlspecialchars($_POST['prix'] ?? '') ?>">
                         <label for="prix">Prix</label>
                     </div>
                 </div>
@@ -60,7 +113,7 @@ require_once 'lib/pdo.php';
                 <option value="2">Peugeot Expert</option>
             </select>
 
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Rechercher</button>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">C'est parti</button>
             <hr class="my-4">
 
 
