fe3d383cea4692fd0bff9c296a46a39c64d7bc90
sophiedannery <dannery.sophie@gmail.com>
2025-02-02 18:10:13 +0100

    ajustement des cards des trajets

--- templates/covoiturages_part.php
+++ templates/covoiturages_part.php
@@ -1,6 +1,6 @@
 <div class="col-md-4 my-2 d-flex">
     <div class="card w-100">
-        <div class="card-body">
+        <div class="card-body d-flex flex-column">
             <h5 class="card-title"><?= $covoiturage["adresse_depart"]; ?> - <?= $covoiturage["adresse_arrivee"]; ?></h5>
 
             <div class="text-start py-4">
@@ -11,8 +11,10 @@
                 <p class="card-text"><i class="bi bi-people-fill"></i> Places disponibles : <?= $covoiturage["place_disponible"]; ?></p>
             </div>
 
-            <!-- Ici le lien à modifier pour récupérer l'id et être amené sur la page du trajet en question -->
-            <a href="covoiturage.php?id=<?= $covoiturage["id"]  ?>" class="btn btn-primary stretched-link w-100">Détails</a>
+            <div class="mt-auto">
+                <!-- Ici le lien à modifier pour récupérer l'id et être amené sur la page du trajet en question -->
+                <a href="covoiturage.php?id=<?= $covoiturage["id"]  ?>" class="btn btn-primary stretched-link w-100">Détails</a>
+            </div>
 
             <div>
                 <p class="card-text text-secondary pt-5 small text-end">Annonce publié le <?= $covoiturage["created_at"]; ?></p>
